c87f9b934a055a7eaa057855dcc042cb81e2db96
Ticiano Herrera <tjherrera@gmail.com>
2024-03-30 13:26:35 -0300

    add Ansible lab

--- labs/terraform/laboratorio/main.tf
+++ labs/terraform/laboratorio/main.tf
@@ -84,7 +84,7 @@ output "subnet_id" {
 resource "aws_instance" "instancia" {
   ami           = "ami-004dac467bb041dc7"
   instance_type = "t2.micro"
-  key_name = "terraform_ec2_key"
+  key_name = aws_key_pair.terraform_ec2_key.key_name
   associate_public_ip_address = true
   subnet_id = aws_subnet.subnet-1.id
   security_groups = [aws_security_group.ssh.id]
@@ -93,6 +93,6 @@ resource "aws_instance" "instancia" {
 #Y por último, le decimos a la instancia que agregue nuestra llave pública de ssh generada antes para poder conectarnos con la llave privada luego. 
 resource "aws_key_pair" "terraform_ec2_key" {
 	key_name = "terraform_ec2_key"
-	public_key = "${file("terra-keys.pub")}"
+	public_key = "${file("ansible.key.pub")}"
 }
 
